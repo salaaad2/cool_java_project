f56d8103938e31449545b88ff5ca4749f281e3e7
JozanLeClerc <bousset.rudy@gmail.com>
2019-12-08 17:38:59 +0100

    Cool java project but it's a guessing game

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String args[])
 	{
-		System.out.println("hello everyone and welcome to my java turorial");
+		Random rand;
+		Scanner scan;
+		int secret_nbr;
+		int guess;
+		int count;
+
+		rand = new Random();
+		scan = new Scanner(System.in);
+		secret_nbr = rand.nextInt(100) + 1;
+		guess = 0;
+		count = 0;
+		System.out.println("Welcome to Guess Game!");
+		while (guess != secret_nbr)
+		{
+			System.out.print("\n" +
+							 "Please guess: ");
+			guess = scan.nextInt();
+			count++;
+			if (guess < secret_nbr)
+				System.out.println("Too low");
+			else if (guess > secret_nbr)
+				System.out.println("Too high");
+		}
+		System.out.println("You won!\n");
+		System.out.println("Secret number: " + secret_nbr + "\n" +
+						   "Number of tries: " + count);
 	}
 }
